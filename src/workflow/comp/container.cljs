b4518d08f564cec0dd79abc47f403e6d84b763f6
jiyinyiyong <jiyinyiyong@gmail.com>
2016-12-17 14:00:17 +0800

    confirm workflow working

--- src/workflow/comp/container.cljs
+++ src/workflow/comp/container.cljs
@@ -2,19 +2,12 @@
 (ns workflow.comp.container
   (:require [hsl.core :refer [hsl]] [quamolit.alias :refer [create-comp group text rect]]))
 
-(defn style-button [guide-text]
-  {:font-size 16,
-   :surface-color (hsl 200 80 50),
-   :w 80,
-   :text-color (hsl 200 80 100),
-   :h 32,
-   :text guide-text})
-
-(defn handle-back [mutate!] (fn [event dispatch] (mutate! :portal)))
-
 (defn render [timestamp store]
   (fn [state mutate! instant tick]
     (comment .log js/console state)
-    (group {:style {}} (text {:style {:color (hsl 0 0 0), :text "DEMO"}}) (rect {}))))
+    (group
+     {:style {}}
+     (text {:style {:text-align "center", :size 20, :fill-style (hsl 0 0 0), :text "DEMO"}})
+     (rect {:style {:y 40, :w 100, :fill-style (hsl 240 80 80), :x 100, :height 60}}))))
 
 (def comp-container (create-comp :container render))
